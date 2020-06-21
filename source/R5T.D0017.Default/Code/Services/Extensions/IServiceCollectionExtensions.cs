de72373ce40b27c855d8f7248699a4c5dafcb78b
MinexAutomation <minex.automation@gmail.com>
2020-06-21 00:05:38 -0700

    Added R5T.Dacia.Extensions (DI extensions) project-reference dependency. Added IServiceCollection extensions.

---
+++ source/R5T.D0017.Default/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+using R5T.Lombardy;
+
+
+namespace R5T.D0017.Default
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="FunctionalVisualStudioProjectFileSerializationModifier"/> implementation of <see cref="IFunctionalVisualStudioProjectFileSerializationModifier"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddFunctionalVisualStudioProjectFileSerializationModifier(this IServiceCollection services,
+            IServiceAction<IStringlyTypedPathOperator> stringlyTypedPathOperatorAction)
+        {
+            services
+                .AddSingleton<IFunctionalVisualStudioProjectFileSerializationModifier, FunctionalVisualStudioProjectFileSerializationModifier>()
+                .Run(stringlyTypedPathOperatorAction)
+                ;
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="FunctionalVisualStudioProjectFileSerializationModifier"/> implementation of <see cref="IFunctionalVisualStudioProjectFileSerializationModifier"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceAction<IFunctionalVisualStudioProjectFileSerializationModifier> AddFunctionalVisualStudioProjectFileSerializationModifierAction(this IServiceCollection services,
+            IServiceAction<IStringlyTypedPathOperator> stringlyTypedPathOperatorAction)
+        {
+            var serviceAction = ServiceAction<IFunctionalVisualStudioProjectFileSerializationModifier>.New(() => services.AddFunctionalVisualStudioProjectFileSerializationModifier(stringlyTypedPathOperatorAction));
+            return serviceAction;
+        }
+    }
+}
